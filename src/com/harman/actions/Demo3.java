a90fbf7f974726eb189ce7ecca12a02244495047
Aashish <ashish.meduri@gmail.com>
2022-04-20 16:49:48 +0530

    commit

--- src/com/harman/actions/Demo3.java
+++ src/com/harman/actions/Demo3.java
@@ -2,7 +2,7 @@ package com.harman.actions;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By; 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -12,20 +12,20 @@ public class Demo3 {
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ameduri\\Downloads\\chromedriver.exe");
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
 		
-		driver.get("https://nasscom.in/");
+		driver.get("https://google.com/");
 		
 		Actions action=new Actions(driver);
-		
-		action.moveToElement(driver.findElement(By.xpath("//a[text()='Membership']"))).pause(1000)
-		.moveToElement(driver.findElement(By.xpath("//a[text()='Become a member']")))
+		action.keyDown(Keys.SHIFT).sendKeys("selenium").keyUp(Keys.SHIFT).pause(1000)
+		.sendKeys(Keys.ARROW_DOWN).pause(1000)
+		.sendKeys(Keys.ARROW_DOWN).pause(1000)
+		.sendKeys(Keys.ARROW_DOWN).pause(1000)
+		.sendKeys(Keys.ENTER)
 		.build().perform();
-				
-		driver.findElement(By.xpath("//a[text()='Membership Benefits']")).click();
-
 	}
 
 }
